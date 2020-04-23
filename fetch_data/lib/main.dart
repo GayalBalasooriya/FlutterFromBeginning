e3bd65d21e5d652ed62311956f94f361d4d91f43
Gayal Balasooriya <baigayal01@gmail.com>
2020-04-24 00:12:17 +0530

    registration logic completed

--- fetch_data/lib/main.dart
+++ fetch_data/lib/main.dart
@@ -47,9 +47,13 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<void> userRegistered() async {
+
+    var uid = '123456';
+    const BASE_URL = 'https://us-central1-authentication-31bdb.cloudfunctions.net/api';
+
     try {
       final response = await http.get(
-        Uri.encodeFull('https://jsonplaceholder.typicode.com/posts'),
+        Uri.encodeFull('$BASE_URL/users/$uid'),
         headers: {
           "Accept" : "application/json"
         }
@@ -62,25 +66,40 @@ class _MyAppState extends State<MyApp> {
         var registeredUser = response.body;
         print(registeredUser);
 
-        if(registeredUser != null) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => HomePage()),
-          );
-        } else {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => RegisterPage()),
-          );
-        }
+//        if(registeredUser != null) {
+//          Navigator.push(
+//
+//            context,
+//            MaterialPageRoute(builder: (context) => HomePage()),
+//          );
+//        } else {
+//          Navigator.push(
+//            context,
+//            MaterialPageRoute(builder: (context) => RegisterPage()),
+//          );
+//        }
         // If the server did return a 200 OK response,
         // then parse the JSON.
         //return Album.fromJson(json.decode(response.body));
 
+//      } else {
+//        // If the server did not return a 200 OK response,
+//        // then throw an exception.
+//        throw Exception('Failed to load data');
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+
+      } else if(response.statusCode == 500) {
+        print("User does not exists");
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => RegisterPage()),
+          );
+
       } else {
-        // If the server did not return a 200 OK response,
-        // then throw an exception.
-        throw Exception('Failed to load data');
+        print("Failed to get data");
       }
 
     } catch(e) {
